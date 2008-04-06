4c8e981960976bfffe771ea42b3653ca39a92d44
Edgar Denny <edgardenny@comcast.net>
2008-04-06 18:41:47 -0400

    Slight change.

--- nutrition/media/javascript/tabs.js
+++ nutrition/media/javascript/tabs.js
@@ -15,6 +15,7 @@
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 // When the page is ready
+
 $(document).ready(function(){
         $("#tabs div").each(function(){
                 $(this).hide();
